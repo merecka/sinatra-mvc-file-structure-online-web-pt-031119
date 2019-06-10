fa36a5226300958511833dba7cc0c6fbc3344c9d
Alex Merecka <merecka@gmail.com>
2019-06-10 20:27:51 +0000

    Automatically backed up by Learn

--- app/models/model.rb
+++ app/models/model.rb
@@ -1,17 +1,4 @@
 class Model
 
-  @@all = []
 
-  attr_accessor :name, :breed, :age
-
-  def initiialize(name, breed, age)
-    @name = name
-    @breed = breed
-    @age = age
-  end
-
-  def self.all
-    @@all << self
-  end
-  
 end
